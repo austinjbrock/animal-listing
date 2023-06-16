c71ee9746a03d49dfb99953c72a93dfa30f762b1
austin brock <austinjbrock@gmail.com>
2023-06-15 21:56:57 -0700

    added npm packages

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
+const {mongoClient, MongoClient} = require("mongodb")
 const express = require('express')
-
+let db;
 const app = express()
+app.set("view engine", "ejs")
+app.set('views', "./views")
 
+app.get("/", async (req,res)=>{
+    const allAnimals = await db.collection('animals').find().toArray()
 
-app.get("/", (req,res)=>{
-  res.send('Welcome to the Homepage')  
+  res.render('home', {allAnimals})
 })
 
 app.get ("/admin",(req,res)=>{
@@ -12,4 +16,16 @@ app.get ("/admin",(req,res)=>{
 } )
 
 
+async function start(){
+  const client = new MongoClient("mongodb://root:root@localhost:27017/AnimalMernApp?&authSource=admin"
+  )
+
+client.connect()
+//global DB variable  
+db = client.db()
+
 app.listen(3000)
+
+}
+
+start()
