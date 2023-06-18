097b2de59702c9f342675697fb134ff36670ff81
austin brock <austinjbrock@gmail.com>
2023-06-17 20:58:15 -0700

    changed nodemon task title

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const {mongoClient, MongoClient} = require("mongodb")
 const express = require('express')
+//global DB variable  
 let db;
-const app = express()
-app.set("view engine", "ejs")
-app.set('views', "./views")
+
+//Remove comments below before restarting server -- start at 29.00
+
+// const app = express()
+// app.set("view engine", "ejs")
+// app.set('views', "./views")
 
 app.get("/", async (req,res)=>{
     const allAnimals = await db.collection('animals').find().toArray()
@@ -12,20 +16,20 @@ app.get("/", async (req,res)=>{
 })
 
 app.get ("/admin",(req,res)=>{
-  res.send('Welcome Admin')
-} )
+  res.send('Welcome Admin') })
 
 
-async function start(){
+async function startServer(){
   const client = new MongoClient("mongodb://root:root@localhost:27017/AnimalMernApp?&authSource=admin"
   )
 
 client.connect()
-//global DB variable  
+
 db = client.db()
 
 app.listen(3000)
 
 }
 
-start()
+
+startServer()
